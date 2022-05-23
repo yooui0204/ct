5b1f454f515f7a7c728aec2c11a633597fc08416
yooui <yooui0204@gmail.com>
2022-05-23 22:03:15 +0900

    .

--- 2635.cpp
+++ 2635.cpp
@@ -3,38 +3,45 @@
 using namespace std;
 
 int N;
+int counter = 0;
 vector<int> v;
 vector<int> answer;
-int size = -1;
-int counter = 0;
 
-void copyVector() {
-	answer.clear();
-	for (int i = 0; i < v.size(); i++)
-		answer.push_back(v[i]);
+void print() {
+    cout << answer.size() << '\n';
+    for (int i = 1; i < answer.size(); i++)
+        cout << answer[i] << ' ';
 }
 
-void makeVector(int N,int i, int start) {
-	if (N - i < 0) {
-		if (answer.size() < v.size()) {
-			copyVector();
-			counter = start;
-		}
-		v.clear();
-		return;
-	}
-	v.push_back(N - i);
-	makeVector(i, N - i, start);
+void copy() {
+    answer.clear();
+    for (int i = 0; i < v.size(); i++) {
+        answer.push_back(v[i]);
+    }
+}
+
+void solve(int a, int b) {
+    if (a - b < 0) {
+        if (answer.size() < v.size())
+            copy();
+        return;
+    }
+    v.push_back(a - b);
+    solve(b, a - b);
+}
+
+void input() {
+    cin >> N;
 }
 
 int main() {
-	cin >> N;
-	for (int i = 1; i <= N; i++) {
-		makeVector(N, i,i);
-	}
-	cout << answer.size()+2 << '\n';
-	cout << N << ' ' << counter << ' ';
-	for (int i = 0; i < answer.size(); i++) {
-		cout << answer[i] << ' ';
-	}
+    input();
+    for (int i = 1; i <= N; i++) {
+        v.clear();
+        v.push_back(N);
+        v.push_back(i);
+        solve(N, i);
+    }
+    print();
+    return 0;
 }
