411bad8345bea5e7c50fa6696a5a318c3c7b4f27
yooui <yooui0204@gmail.com>
2022-06-06 14:21:42 +0900

    606

--- 1620.cpp
+++ 1620.cpp
@@ -1,9 +1,37 @@
 #include <iostream>
-#include <vector>
+#include <unordered_map>
+#include <string>
 using namespace std;
 
-vector<int> 
-
 int main() {
+    cin.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
+    int N, M;
+    cin >> N >> M;
+
+    string strArr[100001];
+
+    string name;
+    unordered_map <string, int> pocketMon;
+    for (int i = 1; i <= N; i++) {
+        cin >> name;
+
+        pocketMon.insert(make_pair(name, i));
+        strArr[i] = name;
+    }
+
+    for (int i = 0; i < M; i++) {
+        string input;
+        cin >> input;
+        if (pocketMon.find(input) != pocketMon.end()) {
+            cout << pocketMon.find(input)->second << "\n";
+        }
+        else {
+            int num = stoi(input);
+            cout << strArr[num] << "\n";
+        }
+    }
+
+    return 0;
 }
