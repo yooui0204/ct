8686d549266098c0141c14ef7c3e688ae1d1dccb
yooui <yooui0204@gmail.com>
2022-06-15 09:58:32 +0900

    615

--- 17626.cpp
+++ 17626.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 int N;
-int dp[50001] = { 0,1 };
+int dp[50001] = { 0 ,1 };
 
 int main() {
 	cin >> N;
-	for (int i = 2; i <= 50000; i++) {
+
+	for (int i = 1; i <= N; i++) {
 		dp[i] = dp[i - 1] + 1;
-		for (int j = 2; j * j < i; j++) {
+		for (int j = 2; j * j <= i; j++) {
 			dp[i] = min(dp[i], dp[i - j * j] + 1);
 		}
 	}
+
 	cout << dp[N];
+	return 0;
 }
